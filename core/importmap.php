4ab89588ee352d3c59aedf8f1edee3e460934932
David Ward <dward@wevad.com>
2025-01-31 08:29:57 -0800

    deps: update vue packages in importmap

--- core/importmap.php
+++ core/importmap.php
@@ -25,26 +25,26 @@ return [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
     'vue' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
         'package_specifier' => 'vue/dist/vue.esm-bundler.js',
     ],
     '@vue/runtime-dom' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@vue/compiler-dom' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@vue/shared' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@vue/runtime-core' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@vue/compiler-core' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@vue/reactivity' => [
-        'version' => '3.5.12',
+        'version' => '3.5.13',
     ],
     '@symfony/ux-vue' => [
         'path' => './vendor/symfony/ux-vue/assets/dist/loader.js',
